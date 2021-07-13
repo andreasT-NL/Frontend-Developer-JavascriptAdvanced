b893bb1a9fa43bc4c116d7d2c31abad47848ba42
andreas <74109335+andreasT-NL@users.noreply.github.com>
2021-07-13 15:54:16 +0200

    Add files via upload

---
+++ Level_18/Javascript_ArrowFunction_01.js
@@ -0,0 +1,24 @@
+// zet de functie om naar een arrow functie
+
+// Deel A:
+const ikRockArrowFunctions2 = () => {
+    console.log("Joe, ik rock de arrow functions!");
+};
+const ikRockArrowFunctions3 = () => console.log("Joe, ik rock de arrow functions!");
+// Deel B:
+const fivePlusSeven = () => console.log(5 + 7);
+// Deel C:
+const myFunction = (a, b) => console.log(a + b);
+// Deel D:
+const addFive = a => console.log(a + 5);
+// Deel E:
+const createObject = () => console.log({greeting: 'hoi'});
+
+ikRockArrowFunctions2()
+ikRockArrowFunctions3()
+fivePlusSeven();
+myFunction(3, 2);
+addFive(3);
+createObject();
+
+
