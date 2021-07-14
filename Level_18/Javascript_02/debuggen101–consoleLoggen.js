6528aaffa834c6aa296499322b9691e894278024
andreas <74109335+andreasT-NL@users.noreply.github.com>
2021-07-14 15:28:41 +0200

    Add files via upload

---
+++ Level_18/Javascript_02/debuggen101–consoleLoggen.js
@@ -0,0 +1,20 @@
+const array = [
+    { name: "N. Armstrong", profession: "spacecowboy", age: 89 },
+    { name: "H. de Haan", profession: "kippen hypnotiseur", age: 59 },
+    { name: "A. Curry", profession: "kikvorsman", age: 32 },
+    { name: "F. Vonk", profession: "slangenmelker", age: 36 },
+    { name: "B. Bunny", profession: "konijnen uitlaatservice", age: 27 },
+    { name: "Dr.Evil", profession: "digital overlord", age: 56 }
+];
+
+for (let person of array) {
+    //hier moeten console.logs komen
+    // console.log("All objects:", person);
+    // console.log("This is:", person.name);
+    // console.log('year of birth:', 2021 - person.age);
+    // console.log(person.name, 'is a', person.profession);
+       if (person.age > 50)
+    return true;
+    console.log(person.age);
+
+};
